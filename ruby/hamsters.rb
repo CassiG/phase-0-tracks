e52c465d9a9d73944c781d927ebc3e6ddb2f107a
Cassi Gallagher <ccgall@gmail.com>
2017-04-17 19:41:09 -0700

    Update strings to integers

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,8 +1,14 @@
 puts "What is your name?"
-username = gets.chomp
+
+def hamster_name
+	puts "Nice to meet you #{username}"
+	username = gets.chomp
+end
+
+hamster_name
 
 puts "How loud do you squeak on a scale of 1-10"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 puts "What color is your fur"
 fur = gets.chomp
@@ -10,15 +16,23 @@ fur = gets.chomp
 puts "Are you looking for a good home? (y/n)"
 adoption = gets.chomp
 
+	if adoption == "y" 
+	puts "We have the perfect home for you"
+	elsif adoption == "n"
+		puts "Maybe next time."
+	else 
+		puts "Please choose y or n"
+
+input = false
 until input 
 	puts "How old are you?"
-	input = gets.chomp
+	input = gets.chomp.to_i
 	
-	if input == input.is_an_int
-		age = true
+	if input == input.is_an_ints
+		puts "That's a great age"
+		input = true
 	elsif input ==  nil
 		puts "Are you sure?"
 	else 
 		puts "How old are you?"
 end
-
