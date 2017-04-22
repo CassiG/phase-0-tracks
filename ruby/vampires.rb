8bbfe7e4511233d3193ab795342a8fe87856534c
Cassi Gallagher <ccgall@gmail.com>
2017-04-22 08:07:40 -0700

    Add 5 user questions in ruby to gather information for vampaire detection software

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+# Program to detect if a new employee is a Vampire
+
+# Question Block
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp 
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health = gets.chomp
