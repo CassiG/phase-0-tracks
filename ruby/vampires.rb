ed1599ff89d77f7f7863a7dafe0bbc8d841e5bce
Cassi Gallagher <ccgall@gmail.com>
2017-04-22 13:04:06 -0700

    rearrange logic block to include the questionarie portion in the loop. Added line breaks for readability

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,55 +1,56 @@
 # Program to detect if a new employee is a Vampire
 
-# Question Block
-
+# Check how many employees will be processed
 puts "How many employees will you be checking"
 employees = gets.chomp.to_i
 
-puts "What is your name?" 
-vampire_name = gets.chomp.downcase
-
-puts "How old are you?" 
-age = gets.chomp.to_i
-
-puts "What year were you born?" 
-year = gets.chomp.to_i
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)" 
-garlic = gets.chomp.downcase
-
-puts "Would you like to enroll in the company’s health insurance? (yes/no)" 
-health = gets.chomp
-
-#Additional Variables for logic block
-
-current_year = Time.new.year
-calculated_birthday = current_year - age
 i = 0
 
 # Logic Block
 
-# While i which is 0 is less than the number of employees
+# While i (which is 0) is less than the number of employees
 while i < employees do
-	
-	#Check first, if they have an obvious vampire name, so we can rule them out IMMEDIATLY
-	if vampire_name == "drake cula" || vampire_name == "tu fang"
-		result = "They are definitely a vampire"
 
-		#Check if their birth year age or they're over 100, hates garlic and waives insurance
-		elsif (calculated_birthday != year || age > 100) && garlic == "no" && health == "no"
-			result =  "Almost certainly a vampire" 
+	#Begin Questionaire 
+		puts "What is your name?" 
+			vampire_name = gets.chomp.downcase
 
-		#Check if birth year matches age or they're over a hundred, and either hates garlic or waives insurance
-		elsif (calculated_birthday != year || age > 100) && (garlic == "no" || health == "no")
-			result =  "Probably a vampire"
+		puts "How old are you?" 
+			age = gets.chomp.to_i
 
-		#Check if birth year matchs age and they're less than a hundred, and either want garlic or insurance 
-		elsif (calculated_birthday == year && age <= 100) && (garlic == "yes" || health == "yes")
-			result =  "Probably not a vampire"
+		puts "What year were you born?" 
+			year = gets.chomp.to_i
 
-		else result =  "Results inconclusive"
-		return result
-	end
+		puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)" 
+			garlic = gets.chomp.downcase
+
+		puts "Would you like to enroll in the company’s health insurance? (yes/no)" 
+			health = gets.chomp
+
+	#Additional variables for checking
+		current_year = Time.new.year
+		calculated_birthday = current_year - age
+
+	#Check to see if employee is a vampire
+		#Check first, if they have an obvious vampire name, so we can rule them out IMMEDIATLY
+		if vampire_name == "drake cula" || vampire_name == "tu fang"
+			result = "They are definitely a vampire\n"
+
+			#Check if their birth year age or they're over 100, hates garlic and waives insurance
+			elsif (calculated_birthday != year || age > 100) && garlic == "no" && health == "no"
+				result =  "Almost certainly a vampire\n" 
+
+			#Check if birth year matches age or they're over a hundred, and either hates garlic or waives insurance
+			elsif (calculated_birthday != year || age > 100) && (garlic == "no" || health == "no")
+				result =  "Probably a vampire\n"
+
+			#Check if birth year matchs age and they're less than a hundred, and either want garlic or insurance 
+			elsif (calculated_birthday == year && age <= 100) && (garlic == "yes" || health == "yes")
+				result =  "Probably not a vampire\n"
+
+			else result =  "Results inconclusive\n"
+		end
+	puts result
+	i += 1
 end
 
-	puts result
