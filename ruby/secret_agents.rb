24fd3f84575bab5d195c409561c90eabd1603cc2
Cassi Gallagher <ccgall@gmail.com>
2017-04-22 12:03:12 -0700

    Add while loop to check for password length

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,61 +1,43 @@
 # Encrypt Method
 
 def encrypt(password)
-	# Grab the first three letters of password
-	first_letter = password[0]
-	second_letter = password[1]
-	third_letter = password[2]
 
-	#Check if any of the letters is a "aa" 
-	     #If so, make it "a". 
-	     #If not print the next letter
-    if first_letter.next == "aa"
-    	encrypt_first = "a"
-    elsif
-    	encrypt_first = first_letter.next
-    end
+	index = 0
 
-    if second_letter.next == "aa"
-    	encrypt_second = "a"
-    elsif
-    	encrypt_second = second_letter.next
-    end
-
-    if third_letter.next == "aa"
-    	encrypt_third = "a"
-    elsif
-    	encrypt_third = third_letter.next
-    end
-
-    #Print out new password
-    encrypt_first + encrypt_second + encrypt_third
+	while index < password.length
+		password[index] = password[index].next
+		index += 1
+	end
 
 end
 
 puts encrypt("abc")
-puts encrypt("zed")
+
+#puts encrypt("zed")
 
 # Decrypt Method
 
-def decrypt(password)
+#def decrypt(password)
 	# Grab the first three letters of password
-	first_letter = password[0]
-	second_letter = password[1]
-	third_letter = password[2]	
+#	first_letter = password[0]
+#	second_letter = password[1]
+#	third_letter = password[2]	
 
 	# Check where a letter is
-	decrypt_first_number = "abcdefghijklmnopqrstuvwxyz".index(first_letter) - 1
-	decrypt_first_letter = "abcdefghijklmnopqrstuvwxyz"[decrypt_first_number]
+#	decrypt_first_number = "abcdefghijklmnopqrstuvwxyz".index(first_letter) - 1
+#	decrypt_first_letter = "abcdefghijklmnopqrstuvwxyz"[decrypt_first_number]
 
-	decrypt_second_number = "abcdefghijklmnopqrstuvwxyz".index(second_letter) - 1
-	decrypt_second_letter = "abcdefghijklmnopqrstuvwxyz"[decrypt_second_number]
+#	decrypt_second_number = "abcdefghijklmnopqrstuvwxyz".index(second_letter) - 1
+#	decrypt_second_letter = "abcdefghijklmnopqrstuvwxyz"[decrypt_second_number]
 
-	decrypt_third_number = "abcdefghijklmnopqrstuvwxyz".index(third_letter) - 1
-	decrypt_third_letter = "abcdefghijklmnopqrstuvwxyz"[decrypt_third_number]	
+#	decrypt_third_number = "abcdefghijklmnopqrstuvwxyz".index(third_letter) - 1
+#	decrypt_third_letter = "abcdefghijklmnopqrstuvwxyz"[decrypt_third_number]	
 
 	#print out new password
-	decrypt_first_letter + decrypt_second_letter + decrypt_third_letter
-end
+#	decrypt_first_letter + decrypt_second_letter + decrypt_third_letter
+#end
 
-puts decrypt("bcd")
-puts decrypt("afe")
+#puts decrypt("bcd")
+#puts decrypt("afe")
+
+#puts decrypt(encrypt("swordfish"))
