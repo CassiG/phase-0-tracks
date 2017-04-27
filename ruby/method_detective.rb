1b57d1e794f88c5d39f28d355692b5dccab4c980
Cassi Gallagher <ccgall@gmail.com>
2017-04-26 21:18:56 -0700

    Add .ord and .count methods on line 34 and 38 from ruby docs

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -31,9 +31,9 @@ puts "The mystery of the missing first letter".delete "T"
 puts "Elementary,    my   dear        Watson!".strip
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
