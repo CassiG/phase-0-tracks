566cdaa817be2457db5d6c1b29bc4bbdaaad9b22
Cassi Gallagher <ccgall@gmail.com>
2017-04-27 21:06:21 -0700

    Add questions for user to answer and added those answers into a hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,26 @@
+#Create empty hash
+
+interior_info = {}
+
 #Ask designer for user information 
 
+puts "What is the client's name?"
+interior_info[:name] = gets.chomp
+
+puts "How old are they?"
+interior_info[:age] = gets.chomp.to_i
+
+puts "How many kids do they have?"
+interior_info[:kids] = gets.chomp.to_i
+
+puts "What is their style preference?"
+interior_info[:style] = gets.chomp
+
+puts "What is their favorite color of blue?"
+interior_info[:color] = gets.chomp
+
 #Put information into a hash
+
+p interior_info
 
 #Print hash back out onto screen 
