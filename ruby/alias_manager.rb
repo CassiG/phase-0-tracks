45c8e1853f875486b69cbc6355e4daebc4f4477c
Cassi Gallagher <ccgall@gmail.com>
2017-04-29 16:36:37 -0700

    Add an until loop to keep asking if they want a fake name until yes or exit is entered

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,41 +3,65 @@
 # Cassi Gallagher
 ###
 
-#Step 1: Get detective's name
-	puts 'Hello Detective! Welcome to the mission. We need to create a fake name for you, please provide your current name to proceed'
+	#Release 2 - Loop through the following choices
 
-	print 'First Name: '
-		first_name = gets.chomp.downcase #add downcase to make characters easier to work with
-	print 'Last Name: '
-		last_name = gets.chomp.downcase 
+	#Step 1: Ask if a detective wants to enter a name.
+	answer = '' 
+	
+	until answer == "yes" do
+		puts 'Hello Detective! Welcome to the mission. Would you like to create a fake name? (Type "yes" to proceed and "exit" to quit)'
+		answer = gets.chomp.downcase
 
-	full_name = first_name + ' ' + last_name
+		if answer == "exit"
+			puts "Good Luck on your mission"
+			break
 
-#Step 2: Create fake name
-#Split the name into an array of words & reverse them
-	split_name = full_name.split(' ').reverse
+		elsif answer == "yes"
+			#Step 2: If yes, get detective's name
+			puts 'We need to create a fake name for you, please provide your current name to proceed'
 
-#Split the letters up into its own array
-	next_letter = split_name.join(' ').split('') #use join to put the name back together before splitting characters
+			print 'First Name: '
+				first_name = gets.chomp.downcase #add downcase to make characters easier to work with
+			print 'Last Name: '
+				last_name = gets.chomp.downcase 
 
-#Advance next vowel	
-	#next_vowel = next_letter.map do |char| #iterate through the string
-		#vowel = ['a', 'e', 'i', 'o', 'u'] #define a vowel
-		#if vowel.include?(char) #at each letter, check if it is a vowel
-			#p char = vowel.rotate #if so, rotate it to the next vowel **This is where I am hung up - checking to see if it IS a vowel, but not knowing what the next step is from there
-		#else 
-			#p char #if not, move on to the next letter
-		#end
-	#end
+			full_name = first_name + ' ' + last_name
 
-#Advance next consonants
+			#Step 3: Create fake name
+			#Split the name into an array of words & reverse them
+			split_name = full_name.split(' ').reverse
 
-## Hide all the code that's making me crazy and use option b, just changes the letters somehow
-	mix_name = next_letter.shuffle.join('')
+			#Split the letters up into its own array
+			next_letter = split_name.join(' ').split('') #use join to put the name back together before splitting characters
 
-#Capitalize and Join the letters back together
-	capitalize_name = mix_name.split.map {|char| char.capitalize}
+			#####
 
-#Step 3: Print new fake name 
-	new_name = capitalize_name.join(' ')
-	print "Your new name is now #{new_name}. You may proceed with your mission, and good luck"
+			#Advance next vowel	
+				#next_vowel = next_letter.map do |char| #iterate through the string
+					#vowel = ['a', 'e', 'i', 'o', 'u'] #define a vowel
+					
+					#if vowel.include?(char) #at each letter, check if it is a vowel
+						#p char = vowel.rotate #if so, rotate it to the next vowel **This is where I am hung up - checking to see if it IS a vowel, but not knowing what the next step is from there
+					#else 
+						#p char #if not, move on to the next letter
+					#end
+				#end
+
+			#Advance next consonants
+
+			####
+
+			## Hide all the code that's making me crazy and use option b, just changes the letters somehow
+			mix_name = next_letter.shuffle.join('')
+
+			#Capitalize and Join the letters back together
+			capitalize_name = mix_name.split.map {|char| char.capitalize}
+
+			#Step 4: Print new fake name 
+			new_name = capitalize_name.join(' ')
+			print "Your new name is now #{new_name}. You may proceed with your mission, and good luck"
+		
+		end
+	end
+	
+	
