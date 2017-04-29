f5be9c83f614d0b7ec09817a19b432acf71b1d19
Cassi Gallagher <ccgall@gmail.com>
2017-04-29 12:28:29 -0700

    Initial commit, write pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+###
+# 5.5 Solo Challange | Ruby Detective
+# Cassi Gallagher
+###
+
+#Step 1: Get detective's name
+#Step 2: Create fake name
+
+#Split the name into an array of words
+#Swap the fist name and the last name 
+#Split the letters up into its own array
+#Change all the vowels to the next vowel
+#Change all the consonants to the next consonants
+#Join the letters back together
+
+#Step 3: Print new fake name 
