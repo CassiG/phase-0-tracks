ddae63adb3ad813a03a0bf9bebf5e9340e903ec8
Cassi Gallagher <ccgall@gmail.com>
2017-04-29 13:15:29 -0700

    Add additional psuedocode for mapping through a string and finding a vowel

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,7 +4,7 @@
 ###
 
 #Step 1: Get detective's name
-puts 'Hello Detective! Welcome to the mission. We need to create a fake name for you, please provide your current name to proceed'
+	puts 'Hello Detective! Welcome to the mission. We need to create a fake name for you, please provide your current name to proceed'
 
 	print 'First Name: '
 		first_name = gets.chomp.downcase #add downcase to make characters easier to work with
@@ -15,15 +15,18 @@ puts 'Hello Detective! Welcome to the mission. We need to create a fake name for
 
 #Step 2: Create fake name
 #Split the name into an array of words & reverse them
-
 	split_name = full_name.split(' ').reverse
 
 #Split the letters up into its own array
-
 	next_letter = split_name.join(' ').split('') #use join to put the name back together before splitting characters
-	p next_letter
 
-#Change all the vowels to the next vowel
+#Advance next vowel
+	vowel = ['a', 'e', 'i', 'o', 'u'] #first, define a vowel
+	
+	# map through each letter in the string
+	# if letter equals a vowel
+	# rotate that vowel to the next vowel 
+	
 #Change all the consonants to the next consonants
 #Join the letters back together
 
