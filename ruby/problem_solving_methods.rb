9ed36b6cc61a8f9bb6ba799507bf271d9f555eaf
Cassi Gallagher <ccgall@gmail.com>
2017-04-30 11:25:08 -0700

    Add nil qualifier to method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -2,9 +2,12 @@ arr = [1, 2, 3, 4, 5]
 
 def array_search(num1, num2)
 	i = 0
-	while i < num1.length
+	while i <= num1.length
 		if num2 == num1[i]
 			puts i 
+			break
+		elsif i == num1.length && num2 != num1[i]
+			puts "nil"
 		end
 		i += 1
 	end
