28ecee64eec2b00d957f95d488db88a9a352b621
Cassi Gallagher <ccgall@gmail.com>
2017-05-01 18:30:46 -0700

    Initial commit, add pseudocode for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+## 6.1 Ruby Intro to Classes
+## Cassi Gallagher
+
+# Design a puppy class
+
+# SPECIES ------------------------
+# Fluffies snuggliereris
+
+# CHARACTERISTICS ----------------
+# Eye Count: 2
+# Leg Count: 4
+# Color: varies
+# Fluffiness level: varies
+# Snuggle Enjoyment: high
+
+# BEHAVIOR -----------------------
+# Snuggle
+# Run
+# Roll
