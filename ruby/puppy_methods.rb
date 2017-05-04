31ef4f24b39858e960fc3b0fa6ee15cc6a6a5bfb
Cassi Gallagher <ccgall@gmail.com>
2017-05-03 20:12:50 -0700

    Add code from pairing session, for release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,7 @@
 #Pair programming Andrea Scott and Cassi Gallagher
 
+#puppy class definition
+
 class Puppy
 
   def fetch(toy)
@@ -7,5 +9,31 @@ class Puppy
     toy
   end
 
-end
+  def speak(num)
+    num = num.to_i
+    num.times do
+      puts "Woof!"
+      end
+  end
 
+  def rollover
+    puts "Rollover!"
+  end
+
+  def dogyears(years)
+    years = years.to_i
+    dog_age = years * 7
+    puts "The puppy is #{dog_age} human years old."
+  end
+
+  def highfive(num)
+    num.times {
+      puts "( ^o^)ノ＼(^_^ )"
+    }
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
+end
