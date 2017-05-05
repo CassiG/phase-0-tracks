d7b02022a7b4bc98ecc6f9886ce4ddf7cfcc4a43
Cassi Gallagher <ccgall@gmail.com>
2017-05-05 07:42:50 -0700

    Add loop to iterate through different initialize methods on line 28

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,11 @@
+
 class Santa	
 	def initialize(gender, ethnicity)
-		puts "Initializing Santa instance ..."
+		#puts "Initializing Santa instance ..."
 		@gender	= gender
 		@ethnicity = ethnicity
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		#@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		#@age = 0
 	end
 	
 	def speak
@@ -14,8 +15,25 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}"
 	end
+
+	def diversity
+		puts "This santa listed they are #{@gender} and #{@ethnicity} on their employment diversity form"
+	end
 end
 
-santa = Santa.new("Female", "Asian")
-santa.speak
-santa.eat_milk_and_cookies("snickerdoodle")
+#santa = Santa.new("Lady Santa")
+#santa.speak
+#santa.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+santa_diversity = [["Female", "Asian"], ["Male", "Latino"], ["Non-binary", "Caucasian"], ["Prefer not to say", "Mixed"]]
+
+santa_diversity.each do |gender, ethnicity|
+		santas << Santa.new(gender, ethnicity)
+end
+
+santas.each do |santa|
+	santa.diversity
+end
+
+
