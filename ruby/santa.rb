9c8f8d3bc2fc994e55b3baee83ae8b7768be6bc1
Cassi Gallagher <ccgall@gmail.com>
2017-05-05 18:33:46 -0700

    Create reindeer_ranking without argument on line 24 to test if the getter and setter method works

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,37 @@
 
 class Santa	
 	def initialize(gender, ethnicity)
-		#puts "Initializing Santa instance ..."
+		puts "Initializing Santa instance ..."
 		@gender	= gender
 		@ethnicity = ethnicity
-		#@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		#@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+
+	#getter methods
+	def age 
+		@age
+	end
+
+	#gets_mad_at argument
+	#set that method so you can write it later
+	#create a method that takes a reindeers name as an argument
+	#iterate through the ranking
+	#if the agrgument equals the name of variable iterating through	
+	#shift  that name to the back 
+
+	def get_mad_at
+		@reindeer_ranking
+	end
+
+	#setter method 
+	def age=(new_age)
+		@age = new_age
+	end
+
+	def get_mad_at=(new_order)
+		@reindeer_ranking = new_order
 	end
 	
 	def speak
@@ -21,19 +47,30 @@ class Santa
 	end
 end
 
-#santa = Santa.new("Lady Santa")
+#Release 0 Initial Method Calls
+santa = Santa.new("Lady Santa", "Santa")
 #santa.speak
 #santa.eat_milk_and_cookies("snickerdoodle")
 
-santas = []
-santa_diversity = [["Female", "Asian"], ["Male", "Latino"], ["Non-binary", "Caucasian"], ["Prefer not to say", "Mixed"]]
+#Release 1 Adding Santa Attributes 
+#santas = []
+#santa_diversity = [["Female", "Asian"], ["Male", "Latino"], ["Non-binary", "Caucasian"], ["Prefer not to say", "Mixed"]]
 
-santa_diversity.each do |gender, ethnicity|
-		santas << Santa.new(gender, ethnicity)
-end
+#santa_diversity.each do |gender, ethnicity|
+	#	santas << Santa.new(gender, ethnicity)
+#end
 
-santas.each do |santa|
-	santa.diversity
-end
+#santas.each do |santa|
+#	santa.diversity
+#end
+
+#Release 2 Changing Attributes with a method
+
+santa.age = santa.age + 1
+puts "Santa is now #{santa.age} old"
+
+#create an array to put the 
+santa.get_mad_at = santa.get_mad_at.rotate
+puts "The new order is now #{santa.get_mad_at}"
 
 
