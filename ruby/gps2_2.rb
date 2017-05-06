a07e4d2865b0bf2ad383c5c75ddd9c9f06b25d56
Cassi Gallagher <ccgall@gmail.com>
2017-05-06 16:34:40 -0700

    Add methods for psuedocode on each step on line 32

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -29,3 +29,65 @@
 # steps: puts the finalized hash.
 # output: printed out hash.
 
+#Release 1:
+puts "
+Welcome to ShppNgLst! Silicon Valley's newest startup with no vowels!!!
+                           You got a list?
+                         Is it for shopping?
+                                Boom.
+                             ShppNgLst!
+Why carry a simple piece of paper when you can show off with your phone?
+                             ShppNgLst."
+
+initial_quantity = 0
+shopping_list = {}
+
+def list_additem()
+  puts 'Type "done" when you\'ve finished adding to your list.'
+  name_input = ""
+  while name_input != "done"
+    puts "Enter an item for the ShppNgLst."
+    name_input = gets.chomp
+    break if name_input == "done"
+    puts "Enter a quantity for that ShppNgLst item."
+    quantity_input = gets.chomp
+
+    shopping_list[name_input] = quantity_input
+  end
+  puts shopping_list
+end
+
+def list_remitem(name_remov)
+  puts 'Type "done" when you\'ve finished removing items from your list.'
+  name_remov = ""
+
+  while name_remov != "done"
+
+    puts "Name an item to remove from your ShppNgLst."
+    name_remov = gets.chomp
+    break if name_remov == "done"
+
+    shopping_list.delete(name_remov)
+  end
+
+  shopping_list.each{|key, value| p "You need to buy #{value} #{key}."}
+end
+
+def list_moditem(name_modif)
+  puts 'Type "done" when you\'ve finished removing items from your list.'
+  name_modif = ""
+
+  while name_modif != "done"
+
+    puts "Name an item to remove from your ShppNgLst."
+    name_modif = gets.chomp
+    break if name_modif == "done"
+
+    shopping_list.delete(name_modif)
+  end
+
+  shopping_list.each{|key, value| p "You need to buy #{value} #{key}."}
+end
+
+puts "Let's begin with a new list."
+
