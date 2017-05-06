64d464aa0ee672b121bd53cf83a9c4b3ed572987
Cassi Gallagher <ccgall@gmail.com>
2017-05-06 16:34:03 -0700

    Add pseudocode for Release 0

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,31 @@
+#Release 0
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # Add item_names to an array.
+  # Add an empty hash.
+  # Add loop to add items to empty hash.
+  # Set default value to 0.
+  # print the list to the console [can you use one of your other methods here?]
+# output: Data type should be a hash with keys with values of 0.
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# Steps: example hash_name[new item name] = new item value
+# output: print the new hash w/ keys & values.
+
+# Method to remove an item from the list
+# input: input a key value to delete.
+# steps:use .delete method to delete a key value pair.
+# output: print a new hash minus deleted values
+
+# Method to update the quantity of an item
+# input: input item name first.  Input updated value.
+# steps: example_hash[key]! = new value
+# output:print a new hash with updated values.
+
+# Method to print a list and make it look pretty
+# input: Finalized hash.
+# steps: puts the finalized hash.
+# output: printed out hash.
+
