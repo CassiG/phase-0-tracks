15e2de1fc7ed79dec8375fab54126dfd1bde9cff
Cassi Gallagher <ccgall@gmail.com>
2017-05-06 17:36:43 -0700

    Add module code and class on line 14

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,32 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+#module Shout
+#	def self.yell_angrily(words)
+#		words + "!!!" + " :("
+#	end
 
-	def self.yell_happily(happy_words)
-		happy_words + "!!!" + " :D"
+#	def self.yell_happily(happy_words)
+#		happy_words + "!!!" + " :D"
+#	end
+#end
+
+#puts Shout.yell_angrily("I'm MAD")
+#puts Shout.yell_happily("YAY")
+
+module Shout
+	def shout(shouting)
+		puts "I am feeling so many emotions that today I just need to shout: #{shouting}"
 	end
 end
 
-puts Shout.yell_angrily("I'm MAD")
-puts Shout.yell_happily("YAY")
+class Angry
+	include Shout
+end
+
+class Happy
+	include Shout
+end
+
+yell_angrily = Angry.new
+puts yell_angrily.shout("I'm MAD")
+
+yell_happily = Happy.new
+puts yell_happily.shout("YAY")
