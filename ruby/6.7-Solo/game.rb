d443d56393a44165e9ca5412e49d3727f0141f2e
Cassi Gallagher <ccgall@gmail.com>
2017-05-07 22:38:58 -0700

    Create method and test for initial user input

--- ruby/6.7-Solo/game.rb
+++ ruby/6.7-Solo/game.rb
@@ -7,32 +7,22 @@
 
 #Pseudocode
 
-#One user can enter a word and another user attempts to guess the word.
-	#Take a word from Player 1
-	#Split that word into an array of letters
-#Check the letter for feedback
-	#If that letter = the letter we’re on (from the loop)
-		#Print blank letters  "_" + then print correct letter + add'l blank letters "_”
-		#Else print all blank letters
-#Join the guesses into a word
-#If the joined answer = p1_word.joined && guess_count <= p1_word.length
-	#Congratulations!
-#If the joined answer does not equal p1_word.joined && guess_count = p1_word.length
-	#Boo You Lose
-##
+#Get initial word from player 1
 
 class GuessingGame
 
-	attr_accessor :p1_word, :p2_guess
 	attr_reader :guess_count, :letter_correct, :win_game
 
 	def initialize
-		@p1_word = ""
-		@p2_guess = ""
 		@guess_count = 0
 		@letter_correct = false
 		@win_game = false
 		@answer = []
+	end
+
+	def initial_word
+		puts "Player 1 enter a word"
+		initial_word = "test"
 	end
 
 	def check_letter
@@ -65,18 +55,3 @@ class GuessingGame
 
 end
 
-game = GuessingGame.new
-
-puts "Player 1 enter a word"
-game.p1_word = gets.chomp
-
-while game.guess_count <= game.p1_word.split("")
-	puts "Player 2 guess a letter"
-	game.p2_guess = gets.chomp
-end
-
-if game.check_letter == true
-	puts game.p2_guess
-elsif puts = " _ "
-	
-end
--- ruby/6.7-Solo/game_spec.rb
+++ ruby/6.7-Solo/game_spec.rb
@@ -4,26 +4,7 @@ describe GuessingGame do
 	let(:game) {GuessingGame.new}
 
 	it "Stores the initial word provided by Player 1" do
-		expect(game.p1_word).to eq "testing"
+		expect(game.initial_word).to eq "test"
 	end
 
-	it "splits initial word provided by Player 1" do
-		expect(game.split_letter).to eq ["t", 'e', 's', 't', 'i', 'n', 'g']
-	end
-
-	it "Stores Player 2 letter guess" do
-		expect(game.p2_guess).to eq "t"
-	end
-
-	#it "Refractor code from line 6 to stores the initial word as a split array" do
-	##	expect(game.p1_word).to eq ["t", 'e', 's', 't', 'i', 'n', 'g']
-	#end
-
-	it "Checks to see if guess letter matches a letter in the word" do
-		expect(game.check_letter).to be true
-	end
-
-	it "Checks to see if they got the answer within the amount of guesses provided" do
-		expect(game.win_game).to be true
-	end
 end
