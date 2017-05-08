e6fe9f2891b964b221767fbd8760eaa3009cd3c6
Cassi Gallagher <ccgall@gmail.com>
2017-05-07 23:17:41 -0700

    Add initial word

--- ruby/6.7-Solo/game.rb
+++ ruby/6.7-Solo/game.rb
@@ -9,49 +9,14 @@
 
 #Get initial word from player 1
 
+
+
 class GuessingGame
 
-	attr_reader :guess_count, :letter_correct, :win_game
+	attr_reader :guess_word
 
 	def initialize
-		@guess_count = 0
-		@letter_correct = false
-		@win_game = false
-		@answer = []
-	end
-
-	def initial_word
-		puts "Player 1 enter a word"
-		initial_word = "test"
-	end
-
-	def check_letter
-		@p1_word.split('').each do |index|
-			if index == @p2_guess
-				@letter_correct = true
-			else 
-				false
-			end
-			return letter_correct
-		end
-	end
-
-	def answer 
-		@answer << @p2_guess
-	end
-
-	def win_game
-
-		while @guess_count <= @p1_word.split('').length 
-			if @p1_word == answer.join('')
-				@answer = true
-			else
-				false
-			end
-			@guess_count += 1
-		end
-
+		@guess_word = "test"
 	end
 
 end
-
--- ruby/6.7-Solo/game_spec.rb
+++ ruby/6.7-Solo/game_spec.rb
@@ -4,7 +4,7 @@ describe GuessingGame do
 	let(:game) {GuessingGame.new}
 
 	it "Stores the initial word provided by Player 1" do
-		expect(game.initial_word).to eq "test"
+		expect(game.guess_word).to eq 'test'
 	end
 
 end
