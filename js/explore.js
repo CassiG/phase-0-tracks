f08aad22b3de4380c7667cddbaefe9ad80ec4da7
Cassi Gallagher <ccgall@gmail.com>
2017-05-10 18:47:49 -0700

    Add pseudocode for initial function to return string

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// Reverse a string
+
+// Create a function to take a string as an argument function(string)
+	// Define an empty string (this will hold the new, reversed string)
+	// Create a loop counting backwards
+	// Assign the new string to each character of the current string, going backwards (string[i])
+	// Return the new string
