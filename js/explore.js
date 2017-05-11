bccab8ca69f903cb4a62f0c6cdb0ad0d8cd3b494
Cassi Gallagher <ccgall@gmail.com>
2017-05-10 18:50:27 -0700

    Add for loop to loop through string backwards

--- js/explore.js
+++ js/explore.js
@@ -2,11 +2,15 @@
 
 // Create a function to take a string as an argument function(string)
 	// Define an empty string (this will hold the new, reversed string)
-	// Create a loop counting backwards
+	// Create a loop counting backwards (Take the string lenght and count backwards by 1)
 	// Assign the new string to each character of the current string, going backwards (string[i])
 	// Return the new string
 // Print string
 
 function reverseString(string) {
 	newString = ""
+
+	for (i = (string.length -1); i >= 0; i--) {
+		
+	}
 }
