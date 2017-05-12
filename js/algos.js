11bafa86bf476fb5532068306797f7fc6e70a847
Cassi Gallagher <ccgall@gmail.com>
2017-05-11 19:59:47 -0700

    Add psudocode for Release 0 find longest phrase

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Algorithm Challenge 
+
+// Create a function that takes an array of words or phrases  
+	// Create an empty longest word string 
+	// Loop through each array item. For each item 
+		// Check if that length of that item is longer than the length of the next item\
+		// If it is store it in the logest word array
+// Return the longest word or phrase in the array.
