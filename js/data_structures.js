30c8ce6a7c7a559dc0ee26738bc5ad40a5d5a7bb
Cassi Gallagher <ccgall@gmail.com>
2017-05-12 19:21:11 -0700

    Add function to loop through and add horses to a hash. Add driver code to call the function

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,13 @@ var horse = ["jeff", "ted", "first", "last"];
 horse.push("josh");
 colors.push("gold");
 
+var horses = {};
+
+function toObject(names, values) {
+  var result = {};
+  for (var i = 0; i < names.length; i++)
+       result[names[i]] = values[i];
+  return result;
+}
+
+console.log(toObject(horse, colors));
