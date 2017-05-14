d12f621403da84bd6c0979b629ffb8ded2c8d0df
Cassi Gallagher <ccgall@gmail.com>
2017-05-14 09:32:27 -0700

    Add driver code to test function

--- js/algos.js
+++ js/algos.js
@@ -2,9 +2,9 @@
 
 // RELEASE 0 | Create a function that takes an array of words or phrases  
 	// Create an empty longest word string 
-	// Start with an empty string - longWord
+	// Start the first word (array[0])
 	// Loop and check each array item
-		// if the first array item is longer than longWord, that item becomes longWord (it is blank so it will be)
+		// if the next array item (array[i] starting at 1) that item becomes longWord 		
 		// On each check after that if the next array item is longWord (which will now become the last item each time),
 		//that word is now longword
 // Return the longest word variable
@@ -19,6 +19,8 @@ function longestWord(array) {
 	} console.log(longWord);
 } 
 
-longestWord(["Word", "Longest Word (Cause it's a phrase)", "Longer"])
+longestWord(["long phrase","longer phrase","longest phrase"])
+longestWord(["long phrase","longest phrase","longer phrase"]);
+longestWord(["longest phrase","long phrase","longer phrase"]);
 
 
