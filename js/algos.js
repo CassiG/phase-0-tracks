71f82a854b63c1c3302f3cee5f31b8d81f5612ef
Cassi Gallagher <ccgall@gmail.com>
2017-05-14 09:29:27 -0700

    Update logic in the for loop

--- js/algos.js
+++ js/algos.js
@@ -1,24 +1,23 @@
 // Algorithm Challenge 
 
-// Create a function that takes an array of words or phrases  
+// RELEASE 0 | Create a function that takes an array of words or phrases  
 	// Create an empty longest word string 
-	// Loop through each array item. For each item 
-		// Check it against the first word. If array[i] (the current word) is longer than the first word
-		// Store that word in a variable
-		// Else store the first word in a variable
+	// Start with an empty string - longWord
+	// Loop and check each array item
+		// if the first array item is longer than longWord, that item becomes longWord (it is blank so it will be)
+		// On each check after that if the next array item is longWord (which will now become the last item each time),
+		//that word is now longword
 // Return the longest word variable
 
 function longestWord(array) {
-	var longWord = ""
-	for (var i = 0; i < array.length; i++) {
-		if (array[i].length > array[0].length){
-			longWord = array[i]; 
-		} else {
-			longWord = array[0];
-		}console.log(longWord);
+	var longWord = array[0]
 
-	} 
-}
+	for (var i = 1; i < array.length; i++) {
+		if (array[i].length > longWord.length) {
+			longWord = array[i];
+		} 
+	} console.log(longWord);
+} 
 
 longestWord(["Word", "Longest Word (Cause it's a phrase)", "Longer"])
 
