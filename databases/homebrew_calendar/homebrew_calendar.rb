a28f10ffec142575ba5f2d0e582e6996f2d06234
Cassi Gallagher <ccgall@gmail.com>
2017-05-21 14:46:37 -0700

    Add program psudocode

---
+++ databases/homebrew_calendar/homebrew_calendar.rb
@@ -0,0 +1,21 @@
+# Homebrew Calendar
+# A simple calendar for homebrewers to know when to brew and when to keg their beer depending on the style of beer
+
+# Database: 
+# beer_type, name, brew month, keg month
+
+# Program: 
+# Ask the user if they'd like to enter a beer, delete a beer, or see the current beer calendar 
+	# If they'd like to enter a beer
+		# Ask what kind of beer they'd like to make
+		# Ask for the name
+		# Display what month to start brewing depending on the month, and when to keg
+		# Store data in a database table
+	# If they'd like to delete a beer
+		# Display calendar
+		# Ask them which number they'd like to delete
+		# Delete by ID
+	# If they'd like to see the current beer calendar
+		# Show Current Calendar
+		# Underneath display the current month and any beers that need to be brewed or kegged
+	
