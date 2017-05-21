d2a0c85eec75bd8f81fc67b2a247684eb0ec5bd4
Cassi Gallagher <ccgall@gmail.com>
2017-05-21 14:52:58 -0700

    Create the homebrew database table

--- databases/homebrew_calendar/homebrew_calendar.rb
+++ databases/homebrew_calendar/homebrew_calendar.rb
@@ -5,6 +5,7 @@
 # beer_type, name, brew month, keg month
 
 # Program: 
+# Create a database to store beer information
 # Ask the user if they'd like to enter a beer, delete a beer, or see the current beer calendar 
 	# If they'd like to enter a beer
 		# Ask what kind of beer they'd like to make
@@ -18,4 +19,21 @@
 	# If they'd like to see the current beer calendar
 		# Show Current Calendar
 		# Underneath display the current month and any beers that need to be brewed or kegged
-	
+
+#Database Creation
+require 'sqlite3'
+homebrew_db = SQLite3::Database.new('homebrew.db')
+
+create_table_cmd = 
+	<<-Homebrew_db
+		CREATE TABLE homebrew (
+			id INTEGER PRIMARY KEY,
+			beer_type VARCHAR(255),
+			name VARCHAR(255), 
+			brew_month VARCHAR(255),
+			keg_month VARCHAR(255),
+			is_ready BOOLEAN
+		)
+	Homebrew_db
+
+homebrew_db.execute(create_table_cmd)
