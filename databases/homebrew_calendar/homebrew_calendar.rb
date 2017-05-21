5dac99206e0c743bf076d1ca2e16def7462d9dfc
Cassi Gallagher <ccgall@gmail.com>
2017-05-21 16:34:02 -0700

    Update delete option to display a list of beers first to choose which one to delete

--- databases/homebrew_calendar/homebrew_calendar.rb
+++ databases/homebrew_calendar/homebrew_calendar.rb
@@ -23,6 +23,8 @@
 #Database Creation
 require 'sqlite3'
 homebrew_db = SQLite3::Database.new('homebrew.db')
+homebrew_db.results_as_hash = true
+
 
 create_table_cmd = 
 	<<-Homebrew_db
@@ -36,46 +38,46 @@ create_table_cmd =
 	Homebrew_db
 
 homebrew_db.execute(create_table_cmd)
-list_beers = homebrew_db.execute("SELECT * FROM homebrew")
 
-#Test Beer
-#homebrew_db.execute('INSERT INTO homebrew (beer_type, name, brew_month, keg_month, is_ready_brew, is_ready_keg) VALUES ("IPA", "Hometown IPA", "May", "July", "true", "false")')
-
-# USER PROGRAM
-
+# Program
 def enter_beer(homebrew_db, beer_type, name, brew_month, keg_month)
 	homebrew_db.execute("INSERT INTO homebrew (beer_type, name, brew_month, keg_month) VALUES (?, ?, ?, ?)", [beer_type, name, brew_month, keg_month] )
 end
 
-# USER INTERFACE
+# Ask User to enter beer
 
-	answer = ''
+answer = ''	
 
-	while answer != 'exit' do
-		puts "Type 'enter' to enter a new beer, 'delete' to delete a beer, or 'view' to view the current beer calendar. Type 'exit' to exit program."
-		answer = gets.chomp.downcase
+while answer != 'exit' do
+	puts "Type 'enter' to enter a new beer, 'delete' to delete a beer, or 'view' to view the current beer calendar. Type 'exit' to exit program."
+	answer = gets.chomp.downcase
 
-		if answer == 'enter'
-			puts "Please enter the style of beer you'd like to brew"
-			beer_type = gets.chomp.downcase
+	if answer == 'enter'
+		puts "Please enter the style of beer you'd like to brew"
+		beer_type = gets.chomp.downcase
 
-			puts "What are you going to call your beer?"
-			name = gets.chomp.downcase
+		puts "What are you going to call your beer?"
+		name = gets.chomp.downcase
 
-			puts "What month would you like to brew"
-			brew_month = gets.chomp.downcase
+		puts "What month would you like to brew"
+		brew_month = gets.chomp.downcase
 
-			puts "What month are you going to keg?"
-			keg_month = gets.chomp.downcase
+		puts "What month are you going to keg?"
+		keg_month = gets.chomp.downcase
 	
-			enter_beer(homebrew_db, beer_type, name, brew_month, keg_month)
-		elsif answer == 'delete'
-			puts "Let's delete a beer\n\n"
-		elsif answer == 'view'
-			puts "Let's view a beer\n\n"
-		elsif answer !='exit' && answer != 'enter' && answer != 'delete' && answer != 'view'
-			puts "Please enter one of the four choices\n\n"
+		enter_beer(homebrew_db, beer_type, name, brew_month, keg_month)
+	elsif answer == 'delete'
+		list_beers = homebrew_db.execute("SELECT * FROM homebrew")
+
+		list_beers.each do |beers|
+			puts "Style: #{beers['beer_type']} | Name: #{beers['name']} | Brew Month: #{beers['brew_month']} | Keg Month: #{beers['keg_month']}"
 		end
 
-		break if answer == 'exit'
-	end	
+	elsif answer == 'view'
+		puts "Let's view a beer\n\n"
+	elsif answer !='exit' && answer != 'enter' && answer != 'delete' && answer != 'view'
+		puts "Please enter one of the four choices\n\n"
+	end
+
+	break if answer == 'exit'
+end	
