fcc3e7670bef05bd99d42348d81a79008c0ba19c
Cassi Gallagher <ccgall@gmail.com>
2017-05-27 15:51:05 -0700

    Clean up code

---
+++ web_dev/sinatra_templating/app.rb
@@ -0,0 +1,31 @@
+# require gems
+require 'sinatra'
+require 'sqlite3'
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+db = SQLite3::Database.new("students.db")
+db.results_as_hash = true
+
+# show students on the home page
+#get '/' do
+ # @students = db.execute("SELECT * FROM students")
+ # erb :home
+#end
+
+#get '/students/new' do
+ # erb :new_student
+#end
+
+## Pair Programming code
+get '/' do 
+  @students = db.execute("SELECT * FROM students")
+  erb :index
+end
+
+# create new students via
+# a form
+post '/' do
+  db.execute("UPDATE students SET diet = ? WHERE name = ?", [params['diet'], params['name']])
+  redirect '/'
+end
