31d34e65e84b43a517c57b1a3d0cb67b544c694e
Cassi Gallagher <ccgall@gmail.com>
2017-05-28 12:53:40 -0700

    Create a button to reveal the hidden message that appears when you enter the message

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -2,4 +2,6 @@ $('#save-message').click(function(event){
 	event.preventDefault();
 	var formMsg = $('#secret-msg').val();
 	$('#secret-msg-reveal').html("<p class='msg-style'>" + formMsg + "</p>");
+	$('.msg-revealed').removeClass('hidden');
+	$('form').hide();
 });
