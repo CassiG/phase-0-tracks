f30531b130664b63b4191e4f640b4a42c9200d6e
Cassi Gallagher <ccgall@gmail.com>
2017-05-28 13:48:47 -0700

    fixed reveal message functionality

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,15 +1,13 @@
 $('#save-message').click(function(event){
 	event.preventDefault();
 	var formMsg = $('#secret-msg').val();
-	$('#secret-msg-reveal').html("<p class='msg-style'>" + formMsg + "</p>");
+	$('#secret-msg-reveal').html("<p>" + formMsg + "</p>");
 	$('.msg-revealed').removeClass('hidden');
 	$('form').hide();
 });
 
 $('#reveal').click(function(){
-	$('#secret-msg-reveal').animate({
-		backgroundColor: '#000'
-	}, 1500);
+	$('#secret-msg-reveal').toggleClass('reveal-msg');
 });
 
 $('#new-message').click(function(){
